bec0e71768b443e5395b3905a23bfa8d046229c0
silvananggia <silvananggia@gmail.com>
2025-11-10 14:05:07 +0700

    update

--- backend-gis-revamp/index.js
+++ backend-gis-revamp/index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => {
   console.log('Server is running on ', port);
